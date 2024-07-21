80c05f99a56bc2136e91dbc11f25c17830d05978
ckulkareppc <chinmay@ppc.ee>
2024-07-21 17:02:37 +0300

    df1_show

--- app.py
+++ app.py
@@ -176,7 +176,9 @@ if check_password():
     if selected_columns1:
         fig1 = px.line(df1, x='Month', y=selected_columns1, title='Google PPC Results - Total', height=600, width=800)
         st.plotly_chart(fig1)
-    st.dataframe(df1.head()) 
+
+    df1_show = df1.astype(str)
+    st.dataframe(df1_show) 
     
     
     
