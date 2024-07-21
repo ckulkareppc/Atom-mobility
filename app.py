1ad3d4055513acc5fd8d950412577fcbcecb32e0
ckulkareppc <chinmay@ppc.ee>
2024-07-21 13:44:35 +0300

    toml to json

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2.service_account import Credentials
 import os
 from dotenv import load_dotenv
+import toml
+import json
 
 load_dotenv()
 
@@ -38,15 +40,18 @@ if check_password():
     
     st.title('Atom Mobility Results Dashboard')
 
-    credentials_file_path = 'atom-404416-23dbcd33bb65.json'
-
-    # Define the scope for the Google Sheets API
-    scopes = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-
-    creds = ServiceAccountCredentials.from_json_keyfile_name('atom-404416-23dbcd33bb65.json', scopes)
+    # Load credentials from environment variable
+    creds_toml = os.getenv('google_sheets_credentials')
+    creds_dict = toml.loads(creds_toml)
     
-        
-    client = gspread.authorize(creds)
+    # Convert TOML dictionary to JSON string and then to a dictionary
+    creds_json = json.dumps(creds_dict)
+    creds_dict = json.loads(creds_json)
+    
+    # Set up the Google Sheets API client
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(credentials)
     
 
     spreadsheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1yEnYDfBF2flJpVhcRHVHcgttRBMaCrUphqBSjoOezns/edit?usp=sharing')
