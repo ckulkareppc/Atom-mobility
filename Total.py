64f9e17be23d1698017c6047daa66c047b93e1eb
ckulkareppc <chinmay@ppc.ee>
2024-07-22 14:57:48 +0300

    Update Total.py

--- Total.py
+++ Total.py
@@ -10,13 +10,14 @@ from st_aggrid import AgGrid
 
 st.title('Atom Mobility Results Dashboard')
 
-# Load credentials from st.secrets
-creds_dict = st.secrets["google_sheets_credentials"]
-    
-# Set up the Google Sheets API client
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-client = gspread.authorize(credentials)
+try:
+    # Your existing code
+    creds_dict = st.secrets["google_sheets_credentials"]
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(credentials)
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 spreadsheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1yEnYDfBF2flJpVhcRHVHcgttRBMaCrUphqBSjoOezns/edit?usp=sharing')
 worksheet = spreadsheet.worksheet("Total")
