e2673f4c0fc6ac36ec643bed5408c8b080d58cd7
ckulkareppc <chinmay@ppc.ee>
2024-07-22 12:39:03 +0300

    Update Total.py

--- Total.py
+++ Total.py
@@ -14,31 +14,6 @@ import json
 from st_aggrid import AgGrid
 
 
-load_dotenv()
-
-# Authentication function
-def check_password():
-    def password_entered():
-        if st.session_state["password"] == st.secrets["general"]["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input("Password", type="password", on_change=password_entered, key="password")
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input("Password", type="password", on_change=password_entered, key="password")
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return True
-
-if check_password():
     
     st.title('Atom Mobility Results Dashboard')
 
