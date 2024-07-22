27c2170afb337626bac68cb5d5af22992f9ddd2f
ckulkareppc <chinmay@ppc.ee>
2024-07-22 15:04:50 +0300

    Update Total.py

--- Total.py
+++ Total.py
@@ -11,7 +11,10 @@ from st_aggrid import AgGrid
 
 st.title('Atom Mobility Results Dashboard')
 # Your existing code
+# Load credentials from st.secrets
 creds_dict = st.secrets["google_sheets_credentials"]
+    
+# Set up the Google Sheets API client
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(credentials)
