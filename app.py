999f7b9b0481b1fb6b6623b9e6fad745eaea36a8
ckulkareppc <chinmay@ppc.ee>
2024-07-22 11:55:48 +0300

    df1 html table

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ if check_password():
     
     fig3 = go.Figure(data=trace_data, layout=layout)
     
-    df1_list = [df1.columns.tolist()] + df1.values.tolist()
+    df1_html_table = df1.to_html()
 
     # User column selection
     st.subheader('Total Results')
@@ -179,7 +179,7 @@ if check_password():
     if selected_columns1:
         fig1 = px.line(df1, x='Month', y=selected_columns1, title='Google PPC Results - Total', height=600, width=800)
         st.plotly_chart(fig1)
-    st.write(df1_list)
+    st.write(df1_html_table, unsafe_allow_html=True)
     
      
     st.subheader('Remarketing Results')
