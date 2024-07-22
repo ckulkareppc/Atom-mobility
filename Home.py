7fa61a8016d14d5b0ebd4b3d1f66cb9c0518be04
ckulkareppc <chinmay@ppc.ee>
2024-07-22 12:33:48 +0300

    loading pages by selection

--- Home.py
+++ Home.py
@@ -1,10 +1,18 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import importlib.util
 
 # Main Home Page
 st.title("Atom Mobility Dashboard")
 
 st.write("Welcome to the Atom Mobility Dashboard! Find your all information here.")
+
+# Function to load a page
+def load_page(page_name):
+    spec = importlib.util.spec_from_file_location(page_name, f'pages/{page_name}.py')
+    page = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(page)
+    page.main()
 
 # Create the navigation menu
 with st.sidebar:
@@ -17,11 +25,14 @@ with st.sidebar:
     )
 
 # Total
-if selected == "Total":
-    st.title("Page 1")
-    st.write("Welcome to Page 1!")
-
-# Page 2
-elif selected == "Page 2":
-    st.title("Page 2")
-    st.write("Welcome to Page 2!")
+if page == "Home":
+    st.title("Home Page")
+    st.write("Welcome to the home page!")
+elif page == "Total":
+    load_page("Total")
+elif page == "Generic":
+    load_page("Generic")
+elif page == "Ride Hailing":
+    load_page("hailing")
+elif page == "Car Rental":
+    load_page("rental")
