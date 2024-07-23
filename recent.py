ee157b704e3826db2cda396749a3c167e45a31d2
ckulkareppc <chinmay@ppc.ee>
2024-07-23 11:55:09 +0300

    Update recent.py

--- recent.py
+++ recent.py
@@ -1,1 +1,36 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import plotly.graph_objs as go
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+from st_aggrid import AgGrid
+
+
+# Load credentials from st.secrets
+creds_dict = st.secrets["google_sheets_credentials"]
+    
+# Set up the Google Sheets API client
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+client = gspread.authorize(credentials)
+
+spreadsheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1em504GqPB_WLWacX7NgWBPhllFPZRf-Q60qwjc4nDZI/edit?usp=sharing')
+worksheet = spreadsheet.worksheet("Atom weekly report")
+data = worksheet.get_all_values()
+
+# Convert the data to a DataFrame
+df = pd.DataFrame(data)
+
+# Optionally, set the first row as the header
+df.columns = df.iloc[0]
+df = df[1:]
+
+# Reset the index
+df.reset_index(drop=True, inplace=True)
+
+df1 = df.iloc[1:3]
+
 
